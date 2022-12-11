00cb6a6e8fa5c146bea826cda7faca63d8cd84c2
aggrey <iluv2code2@gmail.com>
2022-12-11 14:50:13 -0600

    Added Auditing Support for Spring Data GPA

--- src/main/java/com/aggrey/eazy/school/hafifu/EazyschoolApplication.java
+++ src/main/java/com/aggrey/eazy/school/hafifu/EazyschoolApplication.java
@@ -3,11 +3,13 @@ package com.aggrey.eazy.school.hafifu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.aggrey.eazy.school.hafifu.repository")
 @EntityScan("com.aggrey.eazy.school.hafifu.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class EazyschoolApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/aggrey/eazy/school/hafifu/service/ContactService.java
+++ src/main/java/com/aggrey/eazy/school/hafifu/service/ContactService.java
@@ -6,6 +6,7 @@ import com.aggrey.eazy.school.hafifu.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -34,8 +35,6 @@ public class ContactService {
         boolean isSaved = true;
 
         contact.setStatus(EazySchoolConstants.OPEN);
-        contact.setCreatedBy(EazySchoolConstants.ANONYMOUS);
-        contact.setCreatedAt(LocalDateTime.now());
         Contact savedContact = contactRepository.save(contact);
 
         if (null != savedContact && savedContact.getContactId() > 0) {
@@ -46,23 +45,19 @@ public class ContactService {
 
     public List<Contact> findMsgsWithOpenStatus() {
         List<Contact> contactMsgs = contactRepository.findByStatus(EazySchoolConstants.OPEN);
-        return  contactMsgs;
+        return contactMsgs;
     }
 
-    public boolean updateMsgStatus(int contactId, String updatedBy){
+    public boolean updateMsgStatus(int contactId) {
         boolean isUpdated = false;
         Optional<Contact> contact = contactRepository.findById(contactId);
 
         contact.ifPresent(contact1 -> {
             contact1.setStatus(EazySchoolConstants.CLOSE);
-            contact1.setUpdatedBy(updatedBy);
-            contact1.setUpdatedAt(LocalDateTime.now());
         });
 
         Contact updatedContact = contactRepository.save(contact.get());
-        if(null != updatedContact && updatedContact.getUpdatedBy() != null)
-
-        {
+        if (null != updatedContact && updatedContact.getUpdatedBy() != null) {
             isUpdated = true;
         }
 
