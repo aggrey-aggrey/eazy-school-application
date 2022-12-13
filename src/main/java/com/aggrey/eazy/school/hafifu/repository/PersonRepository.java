defe12ded19fbc73bdc5738bbafd4590de571e06
aggrey <iluv2code2@gmail.com>
2022-12-12 20:42:04 -0600

    enabling login user db credentials

--- src/main/java/com/aggrey/eazy/school/hafifu/repository/PersonRepository.java
+++ src/main/java/com/aggrey/eazy/school/hafifu/repository/PersonRepository.java
@@ -3,5 +3,6 @@ package com.aggrey.eazy.school.hafifu.repository;
 import com.aggrey.eazy.school.hafifu.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonRepository  extends JpaRepository<Person, Integer> {
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+    Person readByEmail(String email);
 }
