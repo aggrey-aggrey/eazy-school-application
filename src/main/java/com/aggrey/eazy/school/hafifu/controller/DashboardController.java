d3a25ab5c779c51df0d54c24070bb10068b47817
aggrey <iluv2code2@gmail.com>
2022-12-13 18:41:49 -0600

    added fetching data from DB and display  on the profile page

--- src/main/java/com/aggrey/eazy/school/hafifu/controller/DashboardController.java
+++ src/main/java/com/aggrey/eazy/school/hafifu/controller/DashboardController.java
@@ -1,7 +1,10 @@
 package com.aggrey.eazy.school.hafifu.controller;
 
 import com.aggrey.eazy.school.hafifu.model.Contact;
+import com.aggrey.eazy.school.hafifu.model.Person;
+import com.aggrey.eazy.school.hafifu.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -14,16 +17,24 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Slf4j
 @Controller
 public class DashboardController {
-    @RequestMapping("/dashboard")
-    public String displayDashboard(Model model, Authentication authentication) {
 
-        model.addAttribute("username", authentication.getName());
+    @Autowired
+    PersonRepository personRepository;
+
+
+    @RequestMapping("/dashboard")
+    public String displayDashboard(Model model, Authentication authentication, HttpSession httpSession) {
+
+       Person person = personRepository.readByEmail(authentication.getName());
+        model.addAttribute("username", person.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+        httpSession.setAttribute("loggedInPerson", person);
         return "dashboard.html";
     }
 
--- src/main/java/com/aggrey/eazy/school/hafifu/controller/ProfileController.java
+++ src/main/java/com/aggrey/eazy/school/hafifu/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package com.aggrey.eazy.school.hafifu.controller;
 
 import com.aggrey.eazy.school.hafifu.model.Contact;
+import com.aggrey.eazy.school.hafifu.model.Person;
 import com.aggrey.eazy.school.hafifu.model.Profile;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Slf4j
@@ -17,9 +19,22 @@ import java.util.List;
 public class ProfileController {
 
     @RequestMapping("/displayProfile")
-    public ModelAndView displayProfile(Model model){
+    public ModelAndView displayProfile(Model model, HttpSession httpSession) {
+        Person person = (Person) httpSession.getAttribute("loggedInPerson");
 
         Profile profile = new Profile();
+        profile.setName(person.getName());
+        profile.setMobileNumber(person.getMobileNumber());
+        profile.setEmail(person.getEmail());
+
+        if (person.getAddress() != null && person.getAddress().getAddressId() > 0) {
+            profile.setAddress1(person.getAddress().getAddress1());
+            profile.setAddress2(person.getAddress().getAddress2());
+            profile.setCity(person.getAddress().getCity());
+            profile.setState(person.getAddress().getState());
+            profile.setZipCode(person.getAddress().getZipCode());
+        }
+
         ModelAndView modelAndView = new ModelAndView("profile.html");
         model.addAttribute("profile", profile);
         return modelAndView;
