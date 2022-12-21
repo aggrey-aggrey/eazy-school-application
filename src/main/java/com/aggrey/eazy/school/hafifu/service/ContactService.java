95f221165a5829ae460c1ad294f798a54169ec3c
aggrey <iluv2code2@gmail.com>
2022-12-21 10:58:45 -0600

    Enabled being able to switch spring profiles

--- src/main/java/com/aggrey/eazy/school/hafifu/service/ContactService.java
+++ src/main/java/com/aggrey/eazy/school/hafifu/service/ContactService.java
@@ -25,7 +25,7 @@ public class ContactService {
     @Autowired
     private ContactRepository contactRepository;
 
-    Autowired
+    @Autowired
     EazySchoolProps eazySchoolProps;
 
     public ContactService() {
